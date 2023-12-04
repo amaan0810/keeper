e08e0afed2c71c75d37cb907f867be3e0822c53f
Amaan ansari <amaan.ansari2080@gmail.com>
2023-12-04 23:25:01 +0530

    Updates

---
+++ precache-manifest.f1be4f798822a1c99154706d57070d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97288f3392e5efc37b04c55c032e47d",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "354c2b681938047f4e0f",
+    "url": "/keeper/static/css/main.bd4b3ba3.chunk.css"
+  },
+  {
+    "revision": "e5f0f565e1353504f5af",
+    "url": "/keeper/static/js/2.25f5fd73.chunk.js"
+  },
+  {
+    "revision": "df4f6d4281247f473ea5",
+    "url": "/keeper/static/js/3.ca0aac80.chunk.js"
+  },
+  {
+    "revision": "354c2b681938047f4e0f",
+    "url": "/keeper/static/js/main.3fa8ffc1.chunk.js"
+  },
+  {
+    "revision": "233b75103e850ec779c5",
+    "url": "/keeper/static/js/runtime~main.07b055e1.js"
+  }
+]);
